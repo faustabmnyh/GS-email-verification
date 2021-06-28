e5d00e203be0b4d7bd33933c75aa18bd369c2f62
faustabmnyh <faustaabimanyu@gmail.com>
2021-06-28 22:45:47 +0700

    fix small bug

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ let max = 5;
 
 for (let i = 0; i < max; i++) {
   digits[i].addEventListener("keyup", (e) => {
-    digits[i].style.background = "#d2e6ff";
+    if (digits[i].value.length > 0) {
+      digits[i].style.background = "#d2e6ff";
+      digits[i + 1].focus();
+    } else {
+      digits[i].style.background = "#fff";
+    }
     if (i >= digits.length - 1) return;
-    digits[i + 1].focus();
-    if (e.keyCode === 9) digits[i].focus();
+    if (e.keyCode === 9) {
+      digits[i].focus();
+    }
   });
 }
